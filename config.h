88115a134d8d366b672e3fe2e063a4f78683a26b
Sky Kosiner <sky@skykosiner.co.uk>
2024-08-06 17:30:22 +0100

    Commit that dir baby

--- config.h
+++ config.h
@@ -1,32 +1,31 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 1;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
+static int topbar = 1;      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;    /* -c option; centers dmenu on screen */
+static int min_width = 500; /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"JetBrainsMono Nerd Font:size=15",
-	"Noto Color Emoji:pixelsize=22"
-};
+static const char *fonts[] = {"BerkeleyMono Nerd Font:size=15",
+                              "Noto Color Emoji:pixelsize=22"};
 static const unsigned int bgalpha = 0xe0;
 static const unsigned int fgalpha = OPAQUE;
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt =
+    NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+    /*     fg         bg       */
+    [SchemeNorm] = {"#bbbbbb", "#222222"},
+    [SchemeSel] = {"#eeeeee", "#005577"},
+    [SchemeOut] = {"#000000", "#00ffff"},
 };
 static const unsigned int alphas[SchemeLast][2] = {
-	/*		fgalpha		bgalphga	*/
-	[SchemeNorm] = { fgalpha, bgalpha },
-	[SchemeSel] = { fgalpha, bgalpha },
-	[SchemeOut] = { fgalpha, bgalpha },
+    /*		fgalpha		bgalphga	*/
+    [SchemeNorm] = {fgalpha, bgalpha},
+    [SchemeSel] = {fgalpha, bgalpha},
+    [SchemeOut] = {fgalpha, bgalpha},
 };
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 20;
+static unsigned int lines = 20;
 
 /* -h option; minimum height of a menu line */
 static unsigned int lineheight = 0;
@@ -38,3 +37,5 @@ static unsigned int min_lineheight = 8;
  */
 static const char worddelimiters[] = " ";
 
+/* Size of the window border */
+static unsigned int border_width = 0;
